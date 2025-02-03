b0d774d0f97b57d436359518cb6d0c013a324318
Amir Hilal <amir.hilal@itxi.net>
2025-02-03 13:33:55 +0200

    added  home and login components with routing and animations

---
+++ src/app/animations/route.animations.ts
@@ -0,0 +1,25 @@
+import {
+    trigger,
+    transition,
+    style,
+    animate,
+    query,
+} from '@angular/animations';
+
+export const routeAnimations = trigger('routeAnimations', [
+    // Transition between any routes
+    transition('* <=> *', [
+        query(
+            ':enter',
+            [
+                style({
+                    opacity: 0,
+                }),
+            ],
+            { optional: true },
+        ),
+        query(':enter', [animate('300ms ease-in-out', style({ opacity: 1 }))], {
+            optional: true,
+        }),
+    ]),
+]);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  HostListener,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hilal-supply-front';
+
+  isMobile = signal(true); // Default to true in SSR
+
+  // isLoggedIn = computed(() => this.authService.isLoggedIn());
+  isLoggedIn = computed(() => true);
+
+  ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      this.isMobile.set(window.innerWidth <= 700);
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    if (typeof window !== 'undefined') {
+      this.isMobile.set(event.target.innerWidth <= 700);
+    }
+  }
+
+  prepareRoute(outlet: RouterOutlet): string | undefined {
+    return (
+      outlet &&
+      outlet.activatedRouteData &&
+      outlet.activatedRouteData['animation']
+    );
+  }
 }
