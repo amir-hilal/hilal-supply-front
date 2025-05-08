1bbfd8c7c5c15146c543a6a710ab936c789c3e6c
Amir Hilal <amir.hilal@itxi.net>
2025-05-09 02:01:38 +0300

    added lottie in login layout

--- src/app/layout/login-layout/login-layout.component.ts
+++ src/app/layout/login-layout/login-layout.component.ts
@@ -1,18 +1,25 @@
 import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 import { ViewportService } from '../../services/viewport/viewport.service';
 import { selectAuthReady } from '../../store/auth/auth.selectors';
 
 @Component({
   standalone: true,
   selector: 'app-login-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LottieComponent],
   template: `
     @if (authReady() && viewportReady()) {
       <router-outlet />
     } @else {
-      <div class="app-loading">Loading in login...</div>
+      <div class="app-loading">
+        <ng-lottie
+          [options]="lottieOptions"
+          style="width: 150px; height: 150px; margin: 2rem auto; display: block;"
+        ></ng-lottie>
+      </div>
     }
   `,
 })
@@ -22,4 +29,12 @@ export class LoginLayoutComponent {
 
   viewportReady = this.viewportService.isReady$;
   authReady = computed(() => this.store.selectSignal(selectAuthReady)());
+
+  lottieOptions: AnimationOptions = {
+    path: '/lottie/hilalpines_loader.json',
+  };
+
+  animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
 }
